5d7a9b940e9a9fae6385eb1191c85ba527dbd538
zencodism <marek.bogatek@gmail.com>
2014-10-29 20:12:15 +0100

    Delete run_java_crawler.sh

--- run_java_crawler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-URL="https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/GUEST/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL"
-java -cp /home/www/Doodle/htmlunit-2.15-OSGi.jar:/home/www/Doodle/DCrawler/bin DCrawler $URL
-
